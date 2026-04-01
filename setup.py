75b5176c92dc198966ed805191004a4580d4ba03
Dishita <ds3864@drexel.edu>
2026-04-01 10:15:09 -0400

    logger, exception and utils added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="books_recommender",
+    version="0.1.0",
+    packages=find_packages(),
+    author="Dishia Singh",
+    author_email="ds3864@drexel.edu",
+    install_requires = []
+)
